3034790613bb33a32a45d54e729ea2803340db8d
Danji-ya <erangerang15@gmail.com>
2023-06-11 16:08:00 +0900

    feat: Covert CustomComponent to ReactComponent

---
+++ packages/styled/src/base.types.ts
@@ -0,0 +1,32 @@
+import { CSSObject } from '@emotion/react';
+
+export type Dict<T = any> = Record<string, T>;
+
+export type DOMElements = keyof JSX.IntrinsicElements;
+
+export type Factory = {
+  (tag: DOMElements): DjComponent<DOMElements>;
+};
+
+export type DjComponents = {
+  [T in DOMElements]: DjComponent<T>;
+};
+
+export type DjComponent<T extends React.ElementType> = CustomComponent<T>;
+
+type PropsWithChildren<T> = T & { children?: React.ReactNode | undefined };
+
+interface CustomComponent<T extends React.ElementType> {
+  (
+    props: PropsWithChildren<React.ComponentProps<T>> & DjProps,
+    context?: any,
+  ): JSX.Element | null;
+  propTypes?: React.WeakValidationMap<any>;
+  contextTypes?: React.ValidationMap<any>;
+  defaultProps?: Partial<any>;
+  displayName?: string;
+}
+
+interface DjProps {
+  styles?: CSSObject[];
+}
