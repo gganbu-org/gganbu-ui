a4c6bbb466f2c12e37b14e4a63bb9f8fc358840a
Danji-ya <erangerang15@gmail.com>
2023-07-02 15:41:50 +0900

    fix: 누락된 utils함수 테스트 코드 추가

--- packages/styled/__tests__/utils.test.ts
+++ packages/styled/__tests__/utils.test.ts
@@ -1,4 +1,10 @@
-import { COLORS, getColor, getRandomColor } from '../src/utils';
+import {
+  COLORS,
+  getColor,
+  getRandomColor,
+  isArray,
+  isObject,
+} from '../src/utils';
 
 describe('getColor', () => {
   it('should return the correct color based on index', () => {
@@ -17,5 +23,36 @@ describe('getRandomColor', () => {
   it('should return a random color from the COLORS array', () => {
     const randomColor = getRandomColor();
     expect(COLORS).toContain(randomColor);
+  });
+});
+
+describe('isArray', () => {
+  it('should return true if the value is an array', () => {
+    expect(isArray([])).toBe(true);
+    expect(isArray([1, 2, 3])).toBe(true);
+    expect(isArray(['a', 'b', 'c'])).toBe(true);
+  });
+
+  it('should return false if the value is not an array', () => {
+    expect(isArray(null)).toBe(false);
+    expect(isArray(undefined)).toBe(false);
+    expect(isArray('string')).toBe(false);
+    expect(isArray(123)).toBe(false);
+    expect(isArray({})).toBe(false);
+  });
+});
+
+describe('isObject', () => {
+  it('should return true if the value is an object', () => {
+    expect(isObject({})).toBe(true);
+    expect(isObject({ name: 'John', age: 30 })).toBe(true);
+  });
+
+  it('should return false if the value is not an object', () => {
+    expect(isObject(null)).toBe(false);
+    expect(isObject(undefined)).toBe(false);
+    expect(isObject([])).toBe(false);
+    expect(isObject('string')).toBe(false);
+    expect(isObject(123)).toBe(false);
   });
 });
