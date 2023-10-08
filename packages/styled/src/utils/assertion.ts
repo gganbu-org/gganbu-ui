8778cede4263947a442739929813114bb307fa9f
Danji-ya <erangerang15@gmail.com>
2023-10-08 22:58:24 +0900

    feat(css): functional arguments 기능 추가

--- packages/styled/src/utils/assertion.ts
+++ packages/styled/src/utils/assertion.ts
@@ -2,3 +2,6 @@ export const isArray = (val: any) => Array.isArray(val);
 
 export const isObject = (val: any) =>
   val !== null && typeof val === 'object' && !isArray(val);
+
+export const isFunction = (value: any): value is (...args: any[]) => any =>
+  typeof value === 'function';
