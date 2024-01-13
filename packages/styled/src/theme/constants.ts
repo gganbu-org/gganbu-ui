e01719bf6aa7da42085ccd20a8f2b9e7a5b1764e
Danji-ya <erangerang15@gmail.com>
2024-01-14 01:26:19 +0900

    feat(styled): semantic token을 css variable로 변환하는 기능 추가

--- packages/styled/src/theme/constants.ts
+++ packages/styled/src/theme/constants.ts
@@ -1,8 +1,9 @@
 import { Theme } from '@danji/components';
-import { colors } from '../variables';
+import { colors, sematicTokens } from '../variables';
 
 export const DJ_DEFAULT_THEME = {
   colors,
+  sematicTokens,
   components: Theme,
 } as const;
 
@@ -10,3 +11,16 @@ export const THEME = {
   KEY: 'dj',
   DEFAULT_KEY: '__default',
 } as const;
+
+export const TOKEN_ALIASES = {
+  LIGHT: '_light',
+  DARK: '_dark',
+} as const;
+
+export const TOKEN_PSEUDO_CLASSES = {
+  [TOKEN_ALIASES.DARK]: '[data-theme=dark]',
+} as const;
+
+export type PseudoKeys = keyof typeof TOKEN_PSEUDO_CLASSES;
+
+export const pseudoKeys = Object.values(TOKEN_ALIASES);
