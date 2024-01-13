91d353b45e6ed57ff2b4f11aeab8fd5b095b73ad
Danji-ya <erangerang15@gmail.com>
2024-01-14 01:37:39 +0900

    Revert "feat(utils): nonNull assertion 추가"
    This reverts commit 9117ccf3cc4336e82d96251ab9e0d719d8391371.

--- packages/styled/src/utils/assertion.ts
+++ packages/styled/src/utils/assertion.ts
@@ -1,9 +1,7 @@
-export const isNonNull = <T>(val: T) => val !== null && val !== undefined;
+export const isArray = (val: any) => Array.isArray(val);
 
-export const isArray = <T>(val: T) => Array.isArray(val);
-
-export const isObject = <T>(val: T) =>
-  isNonNull(val) && typeof val === 'object' && !isArray(val);
+export const isObject = (val: any) =>
+  val !== null && typeof val === 'object' && !isArray(val);
 
 export const isFunction = (value: any): value is (...args: any[]) => any =>
   typeof value === 'function';
