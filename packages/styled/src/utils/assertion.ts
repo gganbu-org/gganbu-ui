260ea3f69316ca2ff2d17482d4147fedd24d09a3
Danji-ya <erangerang15@gmail.com>
2024-01-27 09:13:22 +0900

    fix(assertion): type predicate 추가

--- packages/styled/src/utils/assertion.ts
+++ packages/styled/src/utils/assertion.ts
@@ -1,8 +1,9 @@
-export const isNonNull = <T>(val: T) => val !== null && val !== undefined;
+export const isNonNull = <T>(val: T | null | undefined): val is T =>
+  val !== null && val !== undefined;
 
-export const isArray = <T>(val: T) => Array.isArray(val);
+export const isArray = (val: any): val is any[] => Array.isArray(val);
 
-export const isObject = <T>(val: T) =>
+export const isObject = <T>(val: T): val is T =>
   isNonNull(val) && typeof val === 'object' && !isArray(val);
 
 export const isFunction = (value: any): value is (...args: any[]) => any =>
