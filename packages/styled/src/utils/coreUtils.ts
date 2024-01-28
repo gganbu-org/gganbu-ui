e1ba33ec407259e000f24b905af5eb4632459d70
Danji-ya <erangerang15@gmail.com>
2024-01-28 10:26:41 +0900

    feat(util): pipe 함수 추가

--- packages/styled/src/utils/coreUtils.ts
+++ packages/styled/src/utils/coreUtils.ts
@@ -1,6 +1,13 @@
 import { isFunction } from './assertion';
 
+type AnyFunction = (...args: any[]) => any;
+
 export const callIfFunc = <T, U extends any[]>(
   valueOrFunc: T | ((...args: U) => T),
   ...args: U
 ): T => (isFunction(valueOrFunc) ? valueOrFunc(...args) : valueOrFunc);
+
+export const pipe =
+  <T>(f: AnyFunction, ...fs: readonly AnyFunction[]): AnyFunction =>
+  (...as: T[]): T =>
+    fs.reduce((value, func) => func(value), f(...as));
