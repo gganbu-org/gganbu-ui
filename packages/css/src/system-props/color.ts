8b491a7b243279c537c9b195cb9f04524481a6e9
Danji-ya <erangerang15@gmail.com>
2024-04-07 10:46:49 +0900

    feat(css): borderColor system prop 추가

--- packages/css/src/system-props/color.ts
+++ packages/css/src/system-props/color.ts
@@ -4,6 +4,7 @@ import { scales as s } from './scales';
 const color = {
   color: s.colors('color'),
   textColor: s.colors('textColor'),
+  borderColor: s.colors('borderColor'),
 };
 
 export interface ColorCSSProperties {
@@ -15,6 +16,10 @@ export interface ColorCSSProperties {
    * The textColor CSS property sets the foreground color value of an element's text and text decorations, and sets the currentcolor value.
    */
   textColor?: CSS.Property.Color | string;
+  /**
+   * The border-color shorthand CSS property sets the color of an element's border.
+   */
+  borderColor?: CSS.Property.BorderColor | string;
 }
 
 export default color;
