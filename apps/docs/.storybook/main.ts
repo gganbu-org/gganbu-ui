a059401e6711b3721003e96e801ee58bdec9cafc
minzzang144 <shigatsu970704@gmail.com>
2024-05-05 17:40:49 +0900

    feat: storybook 버전 업그레이드
    - storybook v7 -> v8
    - @storybook/testing-library -> @storybook/test
    - .storybook/main.ts findStories 함수 수정
    - .storybook/preview.ts preview.parameters.argTypesRegex 수정

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,28 +1,30 @@
 import fs from 'fs';
-import path from 'path';
+import path, { dirname, join } from 'path';
 import type { StorybookConfig } from '@storybook/react-webpack5';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
 function findStories() {
   const filePattern = /^.*\.stories\.tsx$/;
-  const pkgStoriesPath = `./stories`;
-  const files = fs.readdirSync(`${pkgStoriesPath}`);
+  const pkgStoriesPath = `../stories`;
+  const files = fs.readdirSync(path.resolve(__dirname, pkgStoriesPath));
 
   return files
     .filter((file) => filePattern.test(file))
-    .map((file) => `../${pkgStoriesPath}/${file}`);
+    .map((file) => `${pkgStoriesPath}/${file}`);
 }
 
 const config: StorybookConfig = {
   stories: [...findStories()],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-dark-mode',
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('storybook-dark-mode'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    '@storybook/addon-webpack5-compiler-swc',
   ],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: getAbsolutePath('@storybook/react-webpack5'),
     options: {},
   },
   docs: {
@@ -42,3 +44,7 @@ const config: StorybookConfig = {
   },
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
