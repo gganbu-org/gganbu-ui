b84d93761b35b45cefbd8158312be00c4a2315ba
Danji-ya <erangerang15@gmail.com>
2024-05-26 11:20:26 +0900

    refactor: spinner 컴포넌트 system prop 활용하도록 변경

--- packages/components/src/Spinner/Spinner.tsx
+++ packages/components/src/Spinner/Spinner.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from '@danji/styled';
+import { dj, forwardRef } from '@danji/styled';
 import useSpinner from './useSpinner';
 import { SpinnerProps } from './spinner.types';
 
@@ -7,7 +7,7 @@ const Spinner = forwardRef<'div', SpinnerProps>((props, ref) => {
 
   return (
     <Component ref={ref} {...getSpinnerProps()}>
-      {label}
+      {label && <dj.span srOnly>{label}</dj.span>}
     </Component>
   );
 });
