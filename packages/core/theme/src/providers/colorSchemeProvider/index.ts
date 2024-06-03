d5e5b26b0da807aaf31524e077cba8430438b20f
minzzang144 <shigatsu970704@gmail.com>
2024-06-04 05:00:43 +0900

    fix: export 수정
    - ColorSchemeProvider

--- packages/core/theme/src/providers/colorSchemeProvider/index.ts
+++ packages/core/theme/src/providers/colorSchemeProvider/index.ts
@@ -1,4 +1,7 @@
+import ColorSchemeProvider from './colorSchemeProvider';
+
 export * from './colorScheme.constants';
 export * from './colorScheme.types';
 export * from './colorScheme.utils';
 export * from './colorSchemeContext';
+export { ColorSchemeProvider };
