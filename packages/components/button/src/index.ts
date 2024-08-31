70bc9cf5ff41823030567d3c7dd973f391305ced
Danji-ya <erangerang15@gmail.com>
2024-08-31 15:42:27 +0900

    refactor: export module 방식 통일

--- packages/components/button/src/index.ts
+++ packages/components/button/src/index.ts
@@ -1,3 +1,1 @@
-import Button from './Button';
-
-export { Button };
+export { default as Button } from './Button';
--- packages/components/ripple/src/index.ts
+++ packages/components/ripple/src/index.ts
@@ -1,4 +1,3 @@
-import Ripple from './Ripple';
-import useRipple from './useRipple';
-
-export { Ripple, useRipple };
+export { default as Ripple } from './Ripple';
+export { default as useRipple } from './useRipple';
+export type { RippleElement } from './Ripple.types';
