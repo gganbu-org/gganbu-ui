4916ef15e221ade4216d5f54d2a0b2227486cc78
Danji-ya <erangerang15@gmail.com>
2024-09-14 12:44:46 +0900

    fix: storybook prop type 오류 수정

--- packages/core/styled/src/use-theme/useTheme.types.ts
+++ packages/core/styled/src/use-theme/useTheme.types.ts
@@ -1,11 +1,25 @@
 import type {
-  ButtonVariantProps,
-  SpinnerVariantProps,
+  SpinnerRecipe,
+  ButtonRecipe,
 } from '@gganbu-org/styled-utils/recipes';
 
-interface VariantMap {
-  button: ButtonVariantProps;
-  spinner: SpinnerVariantProps;
+type WithVariantMap = { variantMap: any };
+
+type ExtractIndividualVariant<T> = {
+  [K in keyof T]: T[K] extends Array<infer U> ? U : T[K];
+};
+
+type RecipeVariant<R extends WithVariantMap> = ExtractIndividualVariant<
+  R['variantMap']
+>;
+
+type Recipe<R extends WithVariantMap> = {
+  [K in keyof RecipeVariant<R>]?: RecipeVariant<R>[K] | undefined;
+};
+
+export interface VariantMap {
+  spinner: Recipe<SpinnerRecipe>;
+  button: Recipe<ButtonRecipe>;
 }
 
 export type VariantProps<T extends keyof VariantMap> = VariantMap[T];
