93d53bcc3beded04ed328e165e3e48cf5936dcf7
Danji-ya <erangerang15@gmail.com>
2024-11-23 11:25:11 +0900

    feat: icon-button recipe 연동

--- packages/core/theme/src/recipes/index.ts
+++ packages/core/theme/src/recipes/index.ts
@@ -1,8 +1,10 @@
 import { buttonRecipe } from './button';
+import { iconButtonRecipe } from './iconButton';
 import { SpinnerRecipe } from './spinner';
 
 export const recipes = {
   button: buttonRecipe,
+  iconButton: iconButtonRecipe,
   spinner: SpinnerRecipe,
 };
 
